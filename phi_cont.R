cc6cc5f77517847dbd45b452c4102e65235ac13b
dswatson <david.watson@oii.ox.ac.uk>
2020-10-14 11:57:15 +0100

    no more categorical

---
+++ phi_cont.R
@@ -0,0 +1,53 @@
+# Set working directory
+setwd('~/Documents/UCL')
+
+# Load libraries, register cores
+library(data.table)
+library(ranger)
+library(tidyverse)
+library(doMC)
+registerDoMC(8)
+
+# Set seed
+set.seed(123, kind = "L'Ecuyer-CMRG")
+
+# Import, prep data
+z <- readRDS('./grn/simulations/baseline.rds')
+phis <- readRDS('./grn/simulations/phis.rds')
+
+# Adjacency matrix
+imp <- fread('./grn/adj_mat.csv')
+adj_mat <- ifelse(imp >= 10, 1, 0)
+outdegree <- colSums(adj_mat)
+keep <- which(outdegree >= 100)
+most_causal <- order(outdegree, decreasing = TRUE)[seq_len(10)]
+w <- rep(colSums(imp)[most_causal], each = 1000)
+
+# Random forests
+trn_idx <- c(1:4000, 7001:1000)
+phi_loop <- function(phi_idx) {
+  trn <- sample(trn_idx, 6400)
+  tst <- setdiff(trn_idx, trn)
+  tf <- keep[phi_idx]
+  x <- cbind(z[, c(tf, 334 + which(adj_mat[, tf] == 1))], w)
+  f <- ranger(x = x[trn, ], y = phis[trn, phi_idx], 
+              mtry = ncol(x)/3, num.trees = 500, num.threads = 8)
+  saveRDS(f, paste0('./grn/phi_cont/phi', phi_idx, '.rds'))
+  y_tst <- phis[tst, phi_idx]
+  y_hat <- predict(f, x[tst, ])$predictions
+  out <- data.table(
+    'phi' = paste0('phi', phi_idx),
+     'r2' = cor(y_tst, y_hat)^2,
+    'mse' = mean((y_tst - y_hat)^2)
+  )
+  return(out)
+}
+phi_models_summary <- foreach(g = seq_along(keep), .combine = rbind) %do%
+  phi_loop(g)
+
+
+
+
+
+
+
